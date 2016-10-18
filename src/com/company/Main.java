2921d08aa6e484a236688d3a315af0387d229f26
Anthony J Nave <an375@pvhs-lab112-d05.pvsd.org>
2016-10-18 10:20:49 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        String ab = "New example of String Replace";
+
+        System.out.println(ab.replace('R','O'));
+        System.out.println(ab.replace('N','X'));
+    }
+}
+
+/* Anthony Nave
+Period: 3:
+
+public String replace(char old, char new)
+
+This method replaces all “old char” Strings with a “new char” String-
+
+System.out.println(STRINGNAME.replace('R','O'));*/
